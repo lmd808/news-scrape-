e54d20936a6266cab5d636c37bfaa8dc4000487b
limabean <lditom808@gmail.com>
2020-01-14 14:32:18 -0500

    initial load- login working

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+const app = express();
+require('./config/passport')(passport);
+const logger = require('morgan');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+// Require all models
+// var db = require('./models');
+
+const PORT = process.env.PORT || 3000;
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger('dev'));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static('public'));
+
+// Connect to the Mongo DB
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
+
+mongoose.connect(MONGODB_URI);
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+	session({
+		secret: 'secret',
+		resave: true,
+		saveUninitialized: true
+	})
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+	res.locals.error = req.flash('error');
+	next();
+});
+
+// Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
